584623288ce16bde37d9305f3c515f60ce2255bf
Hayden Durham <hayden.t.durham@gmail.com>
2025-01-02 17:40:32 -0600

    Added .gitignore file

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/hdurham99/skinny-url/internal/storage/db"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+
+	godotenv.Load()
+	rawHosts := os.Getenv("HOSTS")
+	cassHosts := strings.Split(rawHosts, ",")
+	keyspace := os.Getenv("KEYSPACE")
+
+	// CassDB is a Cassandra DB session
+	cassDB, err := db.NewCassandraDB(cassHosts, keyspace)
+	fmt.Println(cassDB, err)
 
 }
--- internal/storage/db/cassandra.go
+++ internal/storage/db/cassandra.go
@@ -1,4 +1,23 @@
 package db
 
+import (
+	"github.com/gocql/gocql"
+)
 
-func 
+type CassandraDB struct {
+	session *gocql.Session
+}
+
+// Initializes the cluster configuration and creates a sesison.
+func NewCassandraDB(hosts []string, keyspace string) (*CassandraDB, error) {
+	// Initialize the Cassandra cluster
+	cluster := gocql.NewCluster(hosts...)
+	cluster.Keyspace = keyspace
+
+	// Create a session
+	session, err := cluster.CreateSession()
+	if err != nil {
+		return nil, err
+	}
+	return &CassandraDB{session: session}, nil
+}
