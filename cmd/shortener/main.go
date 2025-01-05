af94961ebd893cb0bf44413840e9ea12ef1bd861
Hayden Durham <hayden.t.durham@gmail.com>
2025-01-04 19:18:33 -0600

    Added http functionality and redis cache

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,26 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
+	net "net/http"
 	"os"
 	"strings"
 
+	"github.com/hdurham99/skinny-url/internal/http"
+	"github.com/hdurham99/skinny-url/internal/shortener"
+	"github.com/hdurham99/skinny-url/internal/storage/cache"
 	"github.com/hdurham99/skinny-url/internal/storage/db"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	// Load environment variables from .env
-	err := godotenv.Load("../../.env")
+	godotenv.Load("../../.env")
 
-	// Parse the comma-separated HOSTS variable
-	cassHosts := strings.Split(os.Getenv("HOSTS"), ",")
+	// Parse the env varaibles
+	redisHost := os.Getenv("CACHE")
+	cassHosts := strings.Split(os.Getenv("DB"), ",")
 	keyspace := os.Getenv("KEYSPACE")
+	domain := os.Getenv("DOMAIN")
 
-	// Initialize Cassandra DB session
+	// Initialize storage
 	cassDB, err := db.NewCassandraDB(cassHosts, keyspace)
 	if err != nil {
 		fmt.Println("Error creating Cassandra session:", err)
 		return
 	}
-	fmt.Println("Cassandra DB session initialized successfully:", cassDB)
+	redisCache := cache.NewRedisCache(redisHost)
+
+	// Create shortener service
+	shortService := shortener.NewShortenerService(cassDB, redisCache, domain)
+
+	// Create new handler for shortener service
+	handler := http.NewHandler(shortService)
+
+	// Create HTTP router
+	router := http.NewRouter(handler)
+
+	// Start the server
+	log.Fatal(net.ListenAndServe(":8080", router))
 }
